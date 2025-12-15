73432541f80a0facc0fff7b7880da1a256043d13
abhipisaltechnfest-crypto <abhipisal.technfest@gmail.com>
2025-12-15 19:30:24 +0530

    Restore App component with page rendering logic
    Restored the App component and its functionality.

--- App.jsx
+++ App.jsx
@@ -1,35 +1,6 @@
 
 
 
-
-
-// import { useState } from "react";
-// import MainLayout from "./layout/MainLayout";
-// // import Sidebar from "./components/Sidebar";
-// import Dashboard from "./pages/Dashboard";
-// import Leads from "./pages/Leads";
-// import Settings from "./pages/Settings";
-
-// function App() {
-//   const [selectedPage, setSelectedPage] = useState("dashboard");
-
-//   const renderPage = () => {
-//     if (selectedPage === "dashboard") return <Dashboard />;
-//     if (selectedPage === "leads") return <Leads />;
-//     if (selectedPage === "settings") return <Settings />;
-//   };
-
-//   return (
-//     <MainLayout onSelect={setSelectedPage} >
-//       {renderPage()}
-//     </MainLayout>
-//   );
-// }
-
-// export default App;
-
-
-
 import { useState } from "react";
 import MainLayout from "./layout/MainLayout";
 import Dashboard from "./pages/Dashboard";
