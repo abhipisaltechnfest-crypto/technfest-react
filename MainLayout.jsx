80e6ba1f32cb7ec3d1f199d03fdcb1e20785c7d0
abhipisaltechnfest-crypto <abhipisal.technfest@gmail.com>
2025-12-15 19:36:05 +0530

    Refactor MainLayout to use SidebarButton and styles

--- MainLayout.jsx
+++ MainLayout.jsx
@@ -1,66 +1,31 @@
-// import React from "react";
-// import Navbar from "../components/Navbar";
-// import Sidebar from "../components/Sidebar";
 
-// export default function MainLayout({ children }) {
-//   return (
-//     <div className="h-screen flex flex-col">
-      
-//       {/* Navbar */}
-//       <Navbar />
 
-//       <div className="flex flex-1">
-        
-//         {/* Sidebar */}
-//         <Sidebar />
 
-//         {/* Content Area */}
-//         <div className="flex-1 p-6 overflow-y-auto">
-//           {children}
-//         </div>
 
-//       </div>
-//     </div>
-//   );
-// }
+import React from "react";
+import SidebarButton from "../components/SidebarButton";
+import Navbar from "../styles/Navbar";
 
-// import Navbar from "../components/Navbar";
-// import Sidebar from "../components/Sidebar";
-
-// export default function MainLayout({ children, onSelect }) {
-//   return (
-//     <div className="h-screen flex flex-col">
-//       <Navbar />
-
-//       <div className="flex flex-1">
-        
-//         {/* Sidebar only once */}
-//         <Sidebar onSelect={onSelect} />
-
-//         <div className="flex-1 p-6 overflow-y-auto">
-//           {children}
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-import Navbar from "../components/Navbar";
-import Sidebar from "../components/Sidebar";
-
-export default function MainLayout({ children, onSelect }) {
+const MainLayout = ({ children, selectedPage, setSelectedPage }) => {
   return (
-    <div className="h-screen flex flex-col">
+    <div className="flex flex-col h-screen bg-gray-100">
+      {/* Navbar */}
       <Navbar />
 
       <div className="flex flex-1">
-        {/* Sidebar receives onSelect for navigation */}
-        <Sidebar onSelect={onSelect} />
+        {/* Sidebar */}
+        <SidebarButton
+          selectedPage={selectedPage}
+          setSelectedPage={setSelectedPage}
+        />
 
-        <div className="flex-1 p-6 overflow-y-auto">
+        {/* Main Content */}
+        <div className="flex-1 p-6 overflow-auto bg-gray-50">
           {children}
         </div>
       </div>
     </div>
   );
-}
+};
+
+export default MainLayout;
