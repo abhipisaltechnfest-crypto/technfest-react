6492be8adcc18788b582cfe3e19b4f7fa84fe9bf
abhipisaltechnfest-crypto <abhipisal.technfest@gmail.com>
2025-12-24 18:46:29 +0530

    Update Navbar.jsx

--- Navbar.jsx
+++ Navbar.jsx
@@ -3,9 +3,18 @@
 export default function Navbar() {
 
   return (
-    <div className="w-full bg-blue-500 text-white p-4 flex justify-between items-center">
-      <h1 className="text-xl font-bold">Technfest Dashboard</h1>
-      <p>Welcome, User</p>
-    </div>
+
+
+    <header className="
+  bg-gradient-to-r from-indigo-400 via-purple-500 to-pink-400
+  text-white px-6 py-4 flex justify-between items-center
+">
+  <h1 className="text-xl font-semibold">Technfest Dashboard</h1>
+  <p className="text-sm opacity-90 font-semibold">Welcome, User</p>
+</header>
+
   );
 }
+
+
+// new code add      
