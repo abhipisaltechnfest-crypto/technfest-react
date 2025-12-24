498359fde6aafa3e65d401eaf509712c3ded2bc0
abhipisaltechnfest-crypto <abhipisal.technfest@gmail.com>
2025-12-24 18:47:47 +0530

    Update Card.jsx

--- Card.jsx
+++ Card.jsx
@@ -1,16 +1,18 @@
-  
 
 
 export default function Card({ title, value, icon }) {
-  return (
-    <div className="bg-white p-6 rounded-xl shadow border border-black/20 w-full">
-      
-      <div className="flex items-center gap-2">
-      
-        <p className="text-gray-500 font-semibold">{title}</p>
-      </div>
-
-      <h2 className="text-2xl font-bold mt-2">{value}</h2>
+return (<div className="bg-white rounded-xl border-2 border-gray-200 p-6 shadow-md hover:shadow-xl transition">
+  <div className="flex justify-between items-center">
+    <div>
+      <p className="text-sm text-gray-500">{title}</p>
+      <h2 className="text-3xl font-bold text-gray-800">{value}</h2>
     </div>
-  );
+    <div className="bg-blue-100 text-blue-600 p-3 rounded-full">
+      {icon}
+    </div>
+  </div>
+</div>
+
+);
 }
+
